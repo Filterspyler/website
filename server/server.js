66705e42b9c73ad9fdd33b1edde3c2aee13f5370
Joakim <joakim.solvang.christiansen@gmail.com>
2025-02-25 12:50:48 +0100

    testing

--- server/server.js
+++ server/server.js
@@ -6,3 +6,7 @@ const server = express();
 server.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2"]});
 })
+
+server.listen(4000, () => {
+    console.log("Server running on port 4000");
+})
