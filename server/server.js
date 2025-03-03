1da867c3f5a709db3cbe218ccebc2fabb82c6f5a
Joakim <joakim.solvang.christiansen@gmail.com>
2025-03-03 12:57:11 +0100

    Added docker setup for MERN

--- server/server.js
+++ server/server.js
@@ -5,8 +5,8 @@ const server = express();
 
 server.get("/api", (req, res) => {
     res.json({"users": ["user1", "user2"]});
-})
+});
 
-server.listen(4000, () => {
-    console.log("Server running on port 4000");
+server.listen(3000, () => {
+    console.log("Server is now running on port 3000.");
 })
